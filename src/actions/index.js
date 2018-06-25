35bc56f1ae4b453e2022c01a1c9bbee95ee7bd06
Rhys Kearns <rhyskearns176@gmail.com>
2018-06-26 00:15:21 +0100

    Redux Weather App

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_KEY = '29efa3cc7d550158863a72465cc2f573';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},uk`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
